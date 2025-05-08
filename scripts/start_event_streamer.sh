b694aafbad4c14b4619c65c11bbc5d000cbab06f
Sanjay Das <assamese@hotmail.com>
2025-05-08 11:37:05 -0400

    Add files via upload

--- scripts/start_event_streamer.sh
+++ scripts/start_event_streamer.sh
@@ -1,2 +1,2 @@
 python3 ../Python/event_streamer.py > foo.consumer 2>&1 &
-python3 ../Python/aiven_kafka_producer.py > foo.producer 2>&1 &
+python3 ../Python/aiven_kafka_producer.py clicks_file > foo.producer 2>&1 &
